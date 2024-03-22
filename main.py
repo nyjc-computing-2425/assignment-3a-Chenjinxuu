d5f4f073f326a0c11a57810802c9c9d87766748d
Chenjinxuu <160599422+Chenjinxuu@users.noreply.github.com>
2024-03-22 14:22:06 +0000

    final submission
    w

--- main.py
+++ main.py
@@ -4,58 +4,39 @@ from sre_constants import JUMP
 nric = input('Enter an NRIC number: ')
 
 # Type your code below\\
-
-Error_Format = "NRIC format is invalid."
-Error_Nric = "NRIC is invalid"
-Upper_nric = nric.upper()
-prefix = Upper_nric[0]
-string_nric1 = "JZIHGFEDCBA"
-string_nric2 = "XWUTRQPNMLK"
-
+NRIC_Error = "NRIC is invalid."
+nric = nric.upper()
+prefix = nric[0]
 digits = nric[1:8]
-suffix = Upper_nric[8:]
+suffix = nric[8:]
+suffix_stringTS = "JZIHGFEDCBA"
+suffix_stringFG = "XWUTRQPNMLK"
+digit_str = "2765432"
+if len(nric) != 9:
+  print(NRIC_Error)
+elif not digits.isdigit():
+  print(NRIC_Error)
+elif len(suffix) != 1 or not suffix.isalpha():
+    print(NRIC_Error)
+elif prefix not in "TSFG":
+    print(NRIC_Error)
+else:
+     total = 0
+     total =(int(digits[0])* 2 + int(digits[1])* 7 + int(digits[2])* 6+ int(digits[3])* 5 + int(digits[4])* 4 + int(digits[5])* 3 + int(digits[6])* 2)
+     if prefix in "GT":
+         total = total + 4
+     if prefix in "TS":
+       suffix_pos = total % 11
+       suffix_check = suffix_stringTS[suffix_pos]
+       if suffix_check == suffix:
+         print("NRIC is valid.")
+       else:
+         print(NRIC_Error)
 
-if prefix == "T" or prefix == "S" or prefix == "G" or prefix == "F":
- if digits.isdigit() == False:
-  print(Error_Format)
- else:
-  if len(suffix) > 1:
-   print(Error_Nric)
-  else:
-   print("NRIC format is valid.")
-   if prefix == "T":
-    quotient, remainder = divmod(int(nric[1])* 2 + int(nric[2])* 7 + int(nric[3])* 6 + int(nric[4])* 5 + int(nric[5])* 4 + int(nric[6])* 3 + int(nric[7])* 2 + 4, 11)
-    string_nric = "JZIHGFEDCBA"
-    suffix_check = string_nric[remainder]
-    if suffix == suffix_check:
-     print("NRIC is valid.")
-    else:
-     print(Error_Format)
-   else: 
-     if prefix == "S":
-        quotient, remainder = divmod(int(nric[1])* 2 + int(nric[2])* 7 + int(nric[3])* 6 + int(nric[4])* 5 + int(nric[5])* 4 + int(nric[6])* 3 + int(nric[7])* 2, 11)
-        suffix_check1 = string_nric1[remainder]
-        if suffix_check1 == suffix:
+     elif prefix in "FG":
+      suffix_pos = total % 11    
+      suffix_check = suffix_stringFG[suffix_pos]
+      if suffix_check == suffix:
           print("NRIC is valid.")
-        else:
-          print(Error_Nric)
-     else:
-      if prefix == "G":
-        quotient, remainder = divmod(int(nric[1])* 2 + int(nric[2])* 7 + int(nric[3])* 6 + int(nric[4])* 5 + int(nric[5])* 4 + int(nric[6])* 3 + int(nric[7])* 2 + 4, 11)
-        suffix_check = string_nric2[remainder]
-        if suffix_check == suffix:
-            print("NRIC is valid.")
-        else:
-            print(Error_Nric)
-      else:  
-             if prefix == "F":
-              quotient, remainder = divmod(int(nric[1])* 2 + int(nric[2])* 7 + int(nric[3])* 6 + int(nric[4])* 5 + int(nric[5])* 4 + int(nric[6])* 3 + int(nric[7])* 2 , 11)
-              suffix_check = string_nric2[remainder]
-              if suffix_check == suffix:
-                print("NRIC is valid.")
-              else:
-                print(Error_Nric)
-     
-        
-else: 
- print(Error_Nric)
+      else:
+          print(NRIC_Error)
